3ac3657db6987bb614e60c97ecc0db51719eb78b
WillyYang <willy142857@gmail.com>
2018-11-10 11:56:02 +0800

    update api

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -35,75 +35,64 @@
               <table id="example1" class="table table-bordered table-striped">
                 <thead>
                   <tr>
+                    <th>#</th>
                     <th>計畫名稱</th>
                     <th>提案人</th>
                     <th>聯絡信箱</th>
+                    <th>分類</th>
+                    <th>計畫簡介</th>
                     <th>開始時間</th>
                     <th>結束時間</th>
-                    <th>計畫簡介</th>
-                    <th>計畫描述</th>
-                    <th>計畫募資金額</th>
                     <th>已募資金額</th>
+                    <th>計畫募資金額</th>
                     <th>支持人數</th>
+                    <th>計畫描述</th>
                     <th>回饋選項</th>
                     <th>相關網頁</th>
                     <th>管理</th>
                   </tr>
                 </thead>
                 <tbody>
+                  @foreach($projects as $project)
                   <tr>
-                    <td>計畫名稱</td>
-                    <td>提案人</td>
-                    <td>聯絡信箱</td>
-                    <td>開始時間</td>
-                    <td>結束時間</td>
-                    <td>計畫簡介</td>
-                    <td>計畫描述</td>
-                    <td>計畫募資金額</td>
-                    <td>已募資金額</td>
-                    <td>支持人數</td>
-                    <td>回饋選項</td>
-                    <td>相關網頁</td>
+                    <td>{{ $project->id }}</td>
+                    <td>{{ $project->name }}</td>
+                    <td>{{ $project->fundraiser }}</td>
+                    <td>{{ $project->email }}</td>
+                    <td>{{ $project->category->name }}</td>
+                    <td>{{ $project->brief }}</td>
+                    <td>{{ $project->started_at }}</td>
+                    <td>{{ $project->ended_at }}</td>
+                    <td>{{ $project->curr_amount }}</td>
+                    <td>{{ $project->goal_amount }}</td>
+                    <td>{{ $project->backer }}</td>
+                    <td>{{ $project->description }}</td>
+                    <td>{{ $project->feedback }}</td>
+                    <td>{{ $project->img_path }}</td>
+                    <td>{{ $project->relative_web }}</td>
                     <td class="text-center">
-                      <a href="{{ route('projects.edit',$project->id) }}" class="btn btn-xs btn-primary">編輯</a>
-                      <form action="{{ route('projects.destroy',$project->id) }}" method="post" style="display: inline">
+                      <a href="{{ route('projects.edit', $project->id) }}" class="btn btn-xs btn-primary">編輯</a>
+                      <form action="{{ route('projects.destroy', $project->id) }}" method="post" style="display: inline">
                         <button type="submit" class="btn btn-xs btn-danger">刪除</button>
                       </form>
                     </td>
                   </tr>
-                  <tr>
-                    <td>計畫名稱</td>
-                    <td>提案人</td>
-                    <td>聯絡信箱</td>
-                    <td>開始時間</td>
-                    <td>結束時間</td>
-                    <td>計畫簡介</td>
-                    <td>計畫描述</td>
-                    <td>計畫募資金額</td>
-                    <td>已募資金額</td>
-                    <td>支持人數</td>
-                    <td>回饋選項</td>
-                    <td>相關網頁</td>
-                    <td class="text-center">
-                      <a href="" class="btn btn-xs btn-primary">編輯</a>
-                      <form style="display: inline">
-                        <button type="submit" class="btn btn-xs btn-danger">刪除</button>
-                      </form>
-                    </td>
-                  </tr>
+                  @endforeach
                 </tbody>
                 <tfoot>
                 <tr>
+                  <th>#</th>
                   <th>計畫名稱</th>
                   <th>提案人</th>
                   <th>聯絡信箱</th>
+                  <th>分類</th>
+                  <th>計畫簡介</th>
                   <th>開始時間</th>
                   <th>結束時間</th>
-                  <th>計畫簡介</th>
-                  <th>計畫描述</th>
-                  <th>計畫募資金額</th>
                   <th>已募資金額</th>
+                  <th>計畫募資金額</th>
                   <th>支持人數</th>
+                  <th>計畫描述</th>
                   <th>回饋選項</th>
                   <th>相關網頁</th>
                   <th>管理</th>
--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,6 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('projects', 'Api\ProjectController@index');
+Route::get('projects/{project}','Api\ProjectController@show');
