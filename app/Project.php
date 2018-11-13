6099af91a8fda5f930b385387369f8be1b5bd0af
WillyYang <willy142857@gmail.com>
2018-11-13 17:32:53 +0800

    關聯project和category資料表，增加fillable欄位

--- app/Project.php
+++ app/Project.php
@@ -6,5 +6,27 @@ use Illuminate\Database\Eloquent\Model;
 
 class Project extends Model
 {
-    //
+    protected $table = 'projects';
+    protected $fillable = [
+        'fundraiser',
+        'email',
+        'name',
+        'category_id',
+        'brief',
+        'started_at',
+        'ended_at',
+        'curr_amount',
+        'goal_amount',
+        'backer',
+        'description',
+        'feedback',
+        'comment',
+        'relative_web',
+        'img_path',
+    ];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
