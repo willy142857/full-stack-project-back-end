d36875257586504f083c764e5a7debe045b0931f
WillyYang <willy142857@gmail.com>
2018-11-16 13:59:07 +0800

    add CategoryController
    retrurn all categories
    route: api/categories
    {
        "id": 1,
        "category": 分類名稱
    }

--- routes/api.php
+++ routes/api.php
@@ -16,9 +16,11 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::get('projects', 'Api\ProjectsController@index');
+Route::get('projects/index', 'Api\ProjectsController@index');
 
 Route::get('projects/{project}', 'Api\ProjectsController@show');
+
+Route::get('categories', 'Api\CategoryController@index');
 
 Route::post('login', 'Api\AuthController@login');
 Route::post('register', 'Api\AuthController@register');
@@ -27,7 +29,7 @@ Route::get('retrievepassword','Api\EmailController@sendMail');
 Route::middleware('auth:api')->group(function (){
     Route::post('projects', 'Api\ProjectsController@store');
     Route::put('projects/{project}', 'Api\ProjectsContsroller@update');
-    
+
     Route::post('logout', 'Api\AuthController@logout');
     Route::post('refresh', 'Api\AuthController@refresh');
     Route::post('orders', 'Api\OrderController@store');
