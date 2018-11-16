e93f5eee714c2a5d3587aa702edc94d55f5e1eb1
jj861119 <jj861119@gmail.com>
2018-11-16 09:45:13 +0800

    改CORS 和 寄信

--- app/Http/Controllers/Api/EmailController.php
+++ app/Http/Controllers/Api/EmailController.php
@@ -1,23 +1,33 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Api;
 
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Mail;
 
 class EmailController extends Controller
 {
-    public function sendMail(string $email)
+    public function sendMail(Request $request)
     {
+//        $this->validate(
+//            $request,
+//            [
+//                'email'=> 'required|string|email',
+//            ]
+//        );
+
         $data = [
             'title' => '測試使用 Laravel 5 的 Gmail 寄信服務',
         ];
 
 
 
-        Mail::send('emails.welcome', $data, function ($message)use ($email) {
-            $message->to($email);
+        Mail::send('emails.welcome', $data, function ($message)use ($request) {
+            $message->to('jj8611192@gmail.com');
             $message->subject('重設密碼');
         });
+
+        return response()->json(['message' => 'Email successfully send']);
     }
 }
