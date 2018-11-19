fbf9d40f03b872037ae73d21576a79411b9509cb
WillyYang <willy142857@gmail.com>
2018-11-20 01:50:22 +0800

    add comments table
    belongs to Project and User

---
+++ app/Comment.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Comment extends Model
+{
+    protected $table = 'comments';
+
+    protected $fillable =[
+        'user_id',
+        'rating',
+        'comment',
+    ];
+
+    function project()
+    {
+        return $this->belongsTo(Project::class);
+    }
+
+    function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
