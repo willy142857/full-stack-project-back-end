750a1bb5c375fa14c0359714a4dcb2c9fa6236ae
jj861119 <jj861119@gmail.com>
2018-11-19 16:09:55 +0800

    改logout按鈕

--- resources/views/layouts/partials/header.blade.php
+++ resources/views/layouts/partials/header.blade.php
@@ -257,7 +257,15 @@
                                 <a href="#" class="btn btn-default btn-flat">Profile</a>
                             </div>
                             <div class="pull-right">
-                                <a href="#" class="btn btn-default btn-flat">Sign out</a>
+                                {{--<a href="#" class="btn btn-default btn-flat">Sign out</a>--}}
+
+                                <a href="#" class="btn btn-default btn-flat" href="{{ route('logout') }}"
+                                   onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                                    {{ __('Logout') }}
+                                </a>
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                    @csrf
+                                </form>
                             </div>
                         </li>
                     </ul>
