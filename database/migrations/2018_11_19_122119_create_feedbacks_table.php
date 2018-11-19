8fa9c01c819ec20f80fe665c93a51e1865b3f990
WillyYang <willy142857@gmail.com>
2018-11-20 01:52:23 +0800

    add feedbacks table
    belongs to Project

---
+++ database/migrations/2018_11_19_122119_create_feedbacks_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFeedbacksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('feedbacks', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('project_id');
+            $table->date('date');
+            $table->unsignedInteger('price');
+            $table->text('description');
+            $table->unsignedInteger('backer')->default('0');
+            $table->string('img_path')->nullable()->default(null);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('feedbacks');
+    }
+}
