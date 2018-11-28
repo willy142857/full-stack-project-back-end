1e6849db2598ac00466e254d2a9e67a6152706db
B10532036 <B10532036@mail.ntust.edu.tw>
2018-11-28 16:46:36 +0800

    UserController@index 可以拿到所有資料了

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -14,6 +14,18 @@ class UserResource extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'email' => $this->email,
+            'name' => $this->name,
+            'phone' => $this->phone,
+            'description' => $this->description,
+            'sex' =>$this->sex,
+            'address' => $this->address,
+            'birth' => $this->birth,
+            'followingFeedbacks' => FeedbackResource::collection($this->followingFeedbacks),
+            'followingProjects' => ProjectResource::collection($this->followingProjects),
+            'raisingProjects' => ProjectResource::collection($this->raisingProjects),
+        ];
     }
 }
