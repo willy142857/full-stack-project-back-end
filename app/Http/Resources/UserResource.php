a686afde117b02f52ce10cf73632009536390fe7
B10532036 <B10532036@mail.ntust.edu.tw>
2018-12-01 14:22:04 +0800

    上傳大頭照

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -9,7 +9,7 @@ class UserResource extends JsonResource
     /**
      * Transform the resource into an array.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param  \Illuminate\Http\Request $request
      * @return array
      */
     public function toArray($request)
@@ -20,12 +20,13 @@ class UserResource extends JsonResource
             'name' => $this->name,
             'phone' => $this->phone,
             'description' => $this->description,
-            'sex' =>$this->sex,
+            'sex' => $this->sex,
             'address' => $this->address,
             'birth' => $this->birth,
             'followingFeedbacks' => FeedbackResource::collection($this->followingFeedbacks),
             'followingProjects' => ProjectResource::collection($this->followingProjects),
             'raisingProjects' => ProjectResource::collection($this->raisingProjects),
+            'profile_URL' => $this->profile_URL,
         ];
     }
 }
