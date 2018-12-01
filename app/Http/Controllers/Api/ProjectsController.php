fa96812e7cc383f20d1dc5f8565ab3f47cdd61e3
B10532021 <b10532021@gapps.ntust.edu.tw>
2018-12-01 17:25:54 +0800

    回傳comment照片+買單更新資料庫數據

--- app/Http/Controllers/Api/ProjectsController.php
+++ app/Http/Controllers/Api/ProjectsController.php
@@ -37,14 +37,14 @@ class ProjectsController extends Controller
 
             Feedback::create([
                 'project_id' => $project->id,
-                'price' => $feedback['price'] ,
+                'price' => $feedback['price'],
                 'date' => $feedback['date'],
                 'description' => $feedback['description'],
             ]);
         }
 
         Raising::create([
-           'user_id' => auth('api')->user()->id,
+            'user_id' => auth('api')->user()->id,
             'project_id' => $project->id,
         ]);
 
@@ -56,6 +56,16 @@ class ProjectsController extends Controller
 
     public function show(Project $project)
     {
+        foreach ($project->comments as $comment) {
+            $t = file_exists(storage_path('app/public/user/user' . $comment->user_id . '.jpg'));
+
+            if ($t) {
+                $comment->profile_URL = asset('storage/user/user' . $comment->user_id . '.jpg');
+            } else {
+                $comment->profile_URL = null;
+            }
+        }
+
         return new ProjectResource($project);
 //        $project->feedbacks = $project->feedbacks()->get();
 //        $project->comments = $project->comments()->get();
@@ -100,8 +110,6 @@ class ProjectsController extends Controller
     }
 
 
-
-
     public function comment(Request $request)
     {
         $this->validate($request, [
@@ -129,6 +137,14 @@ class ProjectsController extends Controller
             'project_id' => $request->input('project_id'),
             'feedback_id' => $request->input('feedback_id'),
         ]);
+        $feedback_id = $request->input('feedback_id');
+        $data = Feedback::find($feedback_id);
+        $data->backer = $data->backer + 1;
+        $data->save();
+        $project = $data->project;
+        $project->backer = $project->backer + 1;
+        $project->curr_amount = $project->curr_amount + $data->price;
+        $project->save();
         return response()->json([
             'success' => true,
             'user' => auth('api')->user(),
