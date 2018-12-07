327bb832e814a34471034a9c04c618f14706918d
B10532021 <b10532021@gapps.ntust.edu.tw>
2018-12-07 15:50:54 +0800

    修正seeder功能
    直接artisan db:seed就好了

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,6 +12,20 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(UsersTableSeeder::class);
-        $this->call(ExampleProjectTableSeeder::class);
+        $this->call(Example1ProjectTableSeeder::class);
+        $this->call(Example2ProjectTableSeeder::class);
+        $this->call(Example3ProjectTableSeeder::class);
+        $this->call(Example4ProjectTableSeeder::class);
+        $this->call(Example5ProjectTableSeeder::class);
+        $this->call(Example6ProjectTableSeeder::class);
+        $this->call(Example7ProjectTableSeeder::class);
+        $this->call(Example8ProjectTableSeeder::class);
+        $this->call(Example9ProjectTableSeeder::class);
+        $this->call(Example10ProjectTableSeeder::class);
+        $this->call(Example11ProjectTableSeeder::class);
+        $this->call(Example12ProjectTableSeeder::class);
+        $this->call(Example13ProjectTableSeeder::class);
+        $this->call(Example14ProjectTableSeeder::class);
+        $this->call(Example15ProjectTableSeeder::class);
     }
 }
