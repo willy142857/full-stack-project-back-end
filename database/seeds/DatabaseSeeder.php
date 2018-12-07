b2147332633143bfd7619dd2ac1cd3d701d4d082
B10532021 <b10532021@gapps.ntust.edu.tw>
2018-12-07 18:08:39 +0800

    大意了

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,7 +11,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(UsersTableSeeder::class);
         $this->call(ProjectsTableSeeder::class);
         $this->call(Example1ProjectTableSeeder::class);
         $this->call(Example2ProjectTableSeeder::class);
