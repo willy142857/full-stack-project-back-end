09ba5bbf94922199ecc8c983b828d6b8f71000bd
B10532021 <b10532021@gapps.ntust.edu.tw>
2018-12-07 18:23:43 +0800

    git

--- database/seeds/ProjectsTableSeeder.php
+++ database/seeds/ProjectsTableSeeder.php
@@ -17,6 +17,32 @@ class ProjectsTableSeeder extends Seeder
      */
     public function run()
     {
+        Category::truncate();
+
+        $faker = \Faker\Factory::create('zh_TW');
+        $categories = [
+            '音樂',
+            '攝影',
+            '出版',
+            '藝術',
+            '設計',
+            '飲食',
+            '時尚',
+            '教育',
+            '科技',
+            '社會',
+            '表演',
+            '遊戲',
+            '地方創生',
+            '電影動畫',
+            '插畫漫畫',
+        ];
+
+        foreach ($categories as $name) {
+            $category = Category::create([
+                'name' => $name,
+            ]);
+        }
         Comment::truncate();
 
         $faker = \Faker\Factory::create('zh_TW');
